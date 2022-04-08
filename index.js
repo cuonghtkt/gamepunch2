9f696036874b1c2b48bedb422277efe7b4ecbf7c
cuonghtkt <78638881+cuonghtkt@users.noreply.github.com>
2022-04-08 11:40:45 +0700

    update fire bullet

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// module aliases
 const Matter = require('matter-js');
 var Engine = Matter.Engine,
     Render = Matter.Render,
@@ -7,34 +6,33 @@ var Engine = Matter.Engine,
     Body = Matter.Body,
     Events = Matter.Events,
     Composite = Matter.Composite;
-
-// create an engine
 var engine = Engine.create();
 engine.world.gravity.y = 0;
-// create two boxes and a ground
-// var boxA = Bodies.rectangle(400, 200, 80, 80, { mass: 100 });
-// var boxB = Bodies.rectangle(450, 50, 80, 80, { mass: 0 });
-// var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-// Composite.add(engine.world, [boxA, boxB, ground]);
-// run the renderer
-
 
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const { nanoid } = require('nanoid');
-Events.on(engine, 'collisionActive', collision);
-Events.on(engine, 'collisionStart', () => {
-    console.log('collider');
+Events.on(engine, 'collisionStart', CollisionEnter);
+Events.on(engine, "beforeUpdate", () => {
+    var listKey = Object.keys(listBullet);
+    if (listKey.length > 0) {
+
+        listKey.forEach(element => {
+            var positionXGame = listBullet[element].position.x / 100;
+            var positionYGame = listBullet[element].position.y / 100;
+            io.emit("move_bullet", element + "|" + positionXGame + "|" + positionYGame);
+        });
+    }
 });
 var runner = Runner.create();
-
 // run the engine
 Runner.run(runner, engine);
 var listPlayer = [];
+var listBullet = [];
 io.sockets.on('connection', (socket) => {
     //create new player
-    var player = Bodies.circle(0, 0, 125, { name: nanoid() });
+    var player = Bodies.circle(0, 0, 125, { name: nanoid(), density: 0.5, label: "player" });
     console.log("player:" + player.name + "connection...");
     Composite.add(engine.world, [player]);
     io.emit('insert', player.name);
@@ -47,30 +45,64 @@ io.sockets.on('connection', (socket) => {
 
     socket.on('move', (dataGet) => {
         const data = dataGet.split('|');
-        Matter.Body.translate(player, { x: parseFloat(data[0]) * 3, y: parseFloat(data[1]) * 3 });
-        // Body.applyForce(player, { x: player.position.x, y: player.position.y }, { x: 0.003 * data[0], y: 0.003 * data[1] });
-        // if (player.force >= 0.003 * data[0]) {
-        //     player.force = 0.003 * data[0];
-        // }
+        Matter.Body.translate(player, { x: parseFloat(data[0]) * 4, y: parseFloat(data[1]) * 4 });
         io.emit('move', player.name + "|" + (player.position.x / 100) + "|" + (player.position.y / 100));
     });
+    socket.on('attack', (dataGet) => {
+        var degree = parseFloat(dataGet);
+        var radians = (degree * (Math.PI / 180));
+        var vetorAttackX = Math.cos(radians);
+        var vetorAttackY = Math.sin(radians);
+        var offSetXYAttack = 150;
+        var positionXBullet = player.position.x + vetorAttackX * offSetXYAttack;
+        var positionYBullet = player.position.y + vetorAttackY * offSetXYAttack;
+        var buffForce = 5;
 
+        var bullet = Bodies.circle(positionXBullet, positionYBullet, 10, { name: nanoid(), label: "bullet", density: 0.01 });
+        listBullet[bullet.name] = bullet;
+        io.emit("new_bullet", bullet.name + "|" + bullet.position.x + "|" + bullet.position.y);
+        WaitDestroyBullet(bullet.name);
+        Composite.add(engine.world, [bullet]);
+        Body.applyForce(bullet, { x: bullet.position.x, y: bullet.position.y }, {
+            x: Math.cos(radians) / buffForce,
+            y: Math.sin(radians) / buffForce
+        });
+    });
     socket.on('disconnect', () => {
         socket.broadcast.emit('player_disconnect', player.name);
         Composite.remove(engine.world, [player]);
         listPlayer.splice(listPlayer.indexOf(player), 1);
+        console.log("player:" + player.name + "disconnect");
     });
 
 
 
 });
 
-function collision(event) {
+function DeleteBullet(dataGet) {
+    if (listBullet[dataGet] != null) {
+        Composite.remove(engine.world, [listBullet[dataGet]]);
+    }
+    delete listBullet[dataGet];
+    io.emit("delete_bullet", dataGet);
+}
+
+function CollisionEnter(event) {
     const pairs = event.pairs;
     for (let i = 0; i < pairs.length; i++) {
-        io.emit('move', pairs[i].bodyA.name + "|" + (pairs[i].bodyA.position.x / 100) + "|" + (pairs[i].bodyA.position.y / 100));
-        io.emit('move', pairs[i].bodyB.name + "|" + (pairs[i].bodyB.position.x / 100) + "|" + (pairs[i].bodyB.position.y / 100));
+        if (pairs[i].bodyA.label === "bullet") {
+            DeleteBullet(pairs[i].bodyA.name)
+            console.log(pairs[i].bodyB.name + " lost hp")
+        } else if (pairs[i].bodyB.label === "bullet") {
+            DeleteBullet(pairs[i].bodyB.name)
+        }
     }
+}
+
+function WaitDestroyBullet(dataGet) {
+    setTimeout(function() {
+        DeleteBullet(dataGet);
+    }, 3000);
 }
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -78,8 +110,3 @@ app.get('/', (req, res) => {
 http.listen(process.env.PORT || 3000, () => {
     console.log('Connected at 3000');
 });
-// class Player {
-//     constructor() {
-//         this.collider =;
-//     }
-// }
